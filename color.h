905396494b3c25fb880d574b5e35090b8cd1aa66
Martin-Martinez-4 <martmartez4@gmail.com>
2024-10-09 20:04:02 -0700

    Ray Tracing in a Weekend; Listing 20 finished

--- color.h
+++ color.h
@@ -8,8 +8,8 @@ using color = vec3;
 
 void write_color(std::ostream& out, const color& pixel_color){
     auto r = pixel_color.x();
-    auto g = pixel_color.x();
-    auto b = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
 
     // translate the [0,1] component values to the range [0,255].  
     int rbyte = int(255.99 * r);
--- main.cpp
+++ main.cpp
@@ -5,9 +5,50 @@
 
 #include <iostream>
 
-color ray_color(const ray& r){
-    return color(0,0,0);
+// color ray_color(const ray& r){
+//     return color(0, 0, 0);
+// }
+
+// color ray_color_lerp(const ray& r){
+//     // blendedValue=(1−a)⋅startValue+a⋅endValue,
+//     vec3 unit_direction = unit_vector(r.direction());
+//     auto a = 0.5 * (unit_direction.y() + 1.0);
+//     return (1.0-a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+// }
+
+double hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    // double a = dot(r.direction(), r.direction());
+    // double b = -2.0 * dot(r.direction(), oc);
+    // double c = dot(oc, oc) - radius*radius;
+    // double discriminant = b*b - 4*a*c;
+
+    double a = r.direction().length_squared();
+    double h = dot(r.direction(), oc);
+    double c = oc.length_squared() - radius*radius;
+    double discriminant = h*h - a*c;
+    
+    if(discriminant < 0){
+        return -1.0;
+    }else{
+        return (h - std::sqrt(discriminant) ) / a;
+    }
 }
+
+color ray_color(const ray& r){
+
+    double t = hit_sphere(point3(0,0,-1), 0.5, r);
+
+    if(t > 0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
 
 int main(){
 
@@ -32,8 +73,7 @@ int main(){
     auto pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
