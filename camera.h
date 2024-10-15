751d58e447694e952b81062a515561e4e2b5b4c2
Martin-Martinez-4 <martmartez4@gmail.com>
2024-10-15 06:53:54 -0700

    Ray Tracing the Rest; 6.1 start

--- camera.h
+++ camera.h
@@ -59,6 +59,13 @@ private:
 
     int image_height;
     double pixel_sample_scale;
+
+    // square root of number of samples per pixel
+    int sqrt_spp;
+
+    // 1 / sqrt_spp
+    double recip_sqrt_spp;
+
     point3 center;
     point3 pixel00_loc;
     
@@ -79,7 +86,9 @@ private:
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        pixel_sample_scale = 1.0 / samples_per_pixel;
+        sqrt_spp = int(std::sqrt(samples_per_pixel));
+        pixel_sample_scale = 1.0 / (sqrt_spp * sqrt_spp);
+        recip_sqrt_spp = 1.0 / sqrt_spp;
 
         center = lookfrom;
 
@@ -128,10 +137,37 @@ private:
         return ray(ray_origin, ray_direction, ray_time);
     }
 
+    ray get_ray(int i, int j, int s_i, int s_j) const {
+        // Construct a camera ray originating from the defocus disk and directed at a randomly
+        // sampled point around the pixel location i, j for stratified sample square s_i, s_j.
+
+        auto offset = sample_square_stratified(s_i, s_j);
+        auto pixel_sample = pixel00_loc
+                            + ((i + offset.x()) * pixel_delta_u)
+                            + ((j + offset.y()) * pixel_delta_v);
+
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+        auto ray_direction = pixel_sample - ray_origin;
+        auto ray_time = random_double();
+
+        return ray(ray_origin, ray_direction, ray_time);
+    }
+
      vec3 sample_square() const {
         // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
+
+    vec3 sample_square_stratified(int s_i, int s_j) const {
+        // Returns the vector to a random point in the square sub-pixel specified by grid
+        // indices s_i and s_j, for an idealized unit square pixel [-.5,-.5] to [+.5,+.5].
+
+        auto px = ((s_i + random_double()) * recip_sqrt_spp) - 0.5;
+        auto py = ((s_j + random_double()) * recip_sqrt_spp) - 0.5;
+
+        return vec3(px, py, 0);
+    }
+
 
     vec3 sample_disk(double radius) const {
         // Returns a random point in the unit (radius 0.5) disk centered at the origin.
--- main.cpp
+++ main.cpp
@@ -392,12 +392,58 @@ void final_scene() {
     cam.render(world);
 }
 
+void cornell_box_revisit(){
+     hittable_list world;
 
+    auto red   = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+    // Cornell box sides
+    world.add(make_shared<quad>(point3(555,0,0), vec3(0,0,555), vec3(0,555,0), green));
+    world.add(make_shared<quad>(point3(0,0,555), vec3(0,0,-555), vec3(0,555,0), red));
+    world.add(make_shared<quad>(point3(0,555,0), vec3(555,0,0), vec3(0,0,555), white));
+    world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,0,-555), white));
+    world.add(make_shared<quad>(point3(555,0,555), vec3(-555,0,0), vec3(0,555,0), white));
+
+    // Light
+    world.add(make_shared<quad>(point3(213,554,227), vec3(130,0,0), vec3(0,0,105), light));
+
+    // Box 1
+    shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    world.add(box1);
+
+    // Box 2
+    shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    world.add(box2);
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.image_width       = 600;
+    cam.samples_per_pixel = 64;
+    cam.max_depth         = 50;
+    cam.background        = color(0,0,0);
+
+    cam.vfov     = 40;
+    cam.lookfrom = point3(278, 278, -800);
+    cam.lookat   = point3(278, 278, 0);
+    cam.vup      = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
 
 int main(int argc, char* argv[]){
 
-    std::string choices_names[9] = {"Bouncing Spheres", "Checkered Spheres", "Earth", "Perlin Spheres", "Quads", "Simple Lights", "Cornell Box", "Cornell Smoke", "Final Scene"};
-    std::function<void()> choices[9] = {bouncing_spheres, checkered_spheres, earth, perlin_spheres, quads, simple_light, cornell_box, cornell_smoke, final_scene};
+    std::string choices_names[10] = {"Bouncing Spheres", "Checkered Spheres", "Earth", "Perlin Spheres", "Quads", "Simple Lights", "Cornell Box", "Cornell Smoke", "Final Scene", "Cornell Revisit"};
+    std::function<void()> choices[10] = {bouncing_spheres, checkered_spheres, earth, perlin_spheres, quads, simple_light, cornell_box, cornell_smoke, final_scene, cornell_box_revisit};
 
       if(argc != 2){
         std::clog << "Wrong number of arguments; excepted 1, got" << argc -1 << "\n"; 
@@ -439,6 +485,10 @@ int main(int argc, char* argv[]){
         case '8':
             std::clog << choices_names[8] << "\n";
             choices[8]();
+            break;
+        case '9':
+            std::clog << choices_names[9] << "\n";
+            choices[9]();
             break;
         default:
             std::clog << "invalid choice, bye \n";
